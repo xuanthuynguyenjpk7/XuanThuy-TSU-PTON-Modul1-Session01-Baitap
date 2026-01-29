41520e39ad62260b0c4373c5abe96044a4230377
xuanthuynguyenjpk7 <xuanthuynguyenjpk7@gmail.com>
2026-01-29 13:30:37 +0900

    Add files via upload

---
+++ session10/baitap1/baitap4/baitap4.js
@@ -0,0 +1,5 @@
+let firstNumber = Number(prompt('Nhập vào số thứ nhất: '));
+let secondNumber = Number(prompt('Nhập vào số thứ hai: '));
+
+console.log ('Tổng hai số là: ' + (firstNumber + secondNumber));
+alert ('Tổng hai số là: ' + (firstNumber + secondNumber))
