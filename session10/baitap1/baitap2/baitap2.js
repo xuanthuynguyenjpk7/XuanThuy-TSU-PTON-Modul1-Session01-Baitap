55d14cba0bbb01ab60698f1b4e0b4964e6458b3e
xuanthuynguyenjpk7 <xuanthuynguyenjpk7@gmail.com>
2026-01-29 13:15:06 +0900

    Add files via upload

---
+++ session10/baitap1/baitap2/baitap2.js
@@ -0,0 +1,8 @@
+let i = 10;
+let f = 20.5;
+let b = true;
+let s = 'Hà Nội';
+document.writeln(i + "<br>");
+document.writeln(f + "<br>");
+document.writeln(b + "<br>");
+document.writeln(s + "<br>");
