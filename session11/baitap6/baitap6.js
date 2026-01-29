5536e40e8d99e3ddd3e7785c39b87bc87d289471
xuanthuynguyenjpk7 <xuanthuynguyenjpk7@gmail.com>
2026-01-29 14:49:48 +0900

    Add files via upload

---
+++ session11/baitap6/baitap6.js
@@ -0,0 +1,6 @@
+function nhapso() {
+    let a = Number(prompt("Mời bạn nhập vào số a:"));
+    let b = Number(prompt("Mời bạn nhập vào số b:"));
+    let sum = a + b;
+    document.writeln("Tổng của a và b là: " + sum ) ;
+}
