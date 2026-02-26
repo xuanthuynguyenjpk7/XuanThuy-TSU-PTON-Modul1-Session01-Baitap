e1ef09dfcdb31047e19559612ff6855289ffdce2
xuanthuynguyenjpk7 <xuanthuynguyenjpk7@gmail.com>
2026-02-26 15:12:02 +0900

    Delete session16/baitap3 directory

--- session16/baitap3/baitap3.js
+++
@@ -1,8 +0,0 @@
-function sumOftwoNumber(firstNumber, secondNumber) {
-return firstNumber + secondNumber;
-}
-let firstNumber = Number(prompt("Nhập số thứ nhất:"));
-let secondNumber = Number(prompt("Nhập số thứ hai:"));
-let result = sum(firstNumber, secondNumber);
-
-alert ("Tổng của hai số là: " + result);
