6a3b56456d84ee19ff6e6224f8f752a5bc6163cd
xuanthuynguyenjpk7 <xuanthuynguyenjpk7@gmail.com>
2026-02-26 15:12:24 +0900

    Add files via upload

---
+++ session16/baitap3/baitap3.js
@@ -0,0 +1,7 @@
+function sumOftwoNumber(firstNumber, secondNumber) {
+return firstNumber + secondNumber;
+}
+let firstNumber = Number(prompt("Nhập số thứ nhất:"));
+let secondNumber = Number(prompt("Nhập số thứ hai:"));
+let result = sumOftwoNumber(firstNumber, secondNumber);
+alert ("Tổng của hai số là: " + result);
